0d858c59047632701920788d194e937d91205ace
DanielNaro <dnaro@ac.upc.edu>
2021-12-23 16:50:57 +0100

    Test correction

--- unittests/test_SamHeaderSequencesRenamer.py
+++ unittests/test_SamHeaderSequencesRenamer.py
@@ -20,15 +20,11 @@ class TestMain(TestCase):
         SamHeaderSequencesRenamer()
 
         with open('TestOutputs/main_availability_check.txt') as result_file, \
-                open('TestMaterial/expected_main_availability_check_variant2.txt') as expected_file:
+                open('TestMaterial/expected_main_availability_check.txt') as expected_file:
             result = result_file.read()
             expected = expected_file.read()
 
-            if result == expected:
-                return
-            with open('TestMaterial/expected_main_availability_check_variant2.txt') as expected_file_variant2:
-                expected_variant = expected_file_variant2.read()
-                self.assertEqual(expected_variant, result)
+            self.assertEqual(expected, result)
 
     def test_check_availability_empty(self):
         sys.argv = [
@@ -43,7 +39,12 @@ class TestMain(TestCase):
         with open('TestMaterial/expected_main_availability_empty_response.txt') as expected_file:
             result = err.getvalue().strip()
             expected = expected_file.read()
-            self.assertEqual(expected, result)
+
+            if result == expected:
+                return
+            with open('TestMaterial/expected_main_availability_empty_response_variant2.txt') as expected_file_variant2:
+                expected_variant = expected_file_variant2.read()
+                self.assertEqual(expected_variant, result)
 
     def test_check_availability_sequences(self):
         sys.argv = [
